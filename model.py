a22c7aa4f99ac0da9500c19f04fae9dc4edce345
Stas Ahtik <stasahtik@gmail.com>
2021-05-05 14:55:45 +0200

    Krovni model

--- model.py
+++ model.py
@@ -1,6 +1,44 @@
 STEVILO_DOVOLJENIH_NAPAK = 10
 PRAVILNA_CRKA, PONOVLJENA_CRKA, NAPACNA_CRKA = '+', 'o', '-'
+ZACETEK = "s"
 ZMAGA, PORAZ = 'w', 'x'
+
+class Vislice:
+    def __init__(self):
+        self.igre = {}
+        self.max_id = 0
+
+    def prost_id_igre(self):
+        self.max_id += 1
+        return self.max_id
+
+    """ Druga možnost:
+    def prost_id_igre_drugace(self):
+        if not self.igre: return 0
+        m = max(self.igre.keys())
+        return m + 1 
+    Če je self.igre prazen, to ne deluje,
+    zato smo dodali prvo vrstico.
+    """
+
+    def nova_igra(self):
+        nov_id = self.prost_id_igre()
+        sveza_igra = nova_igra(bazen_besed)
+
+        self.igre[nov_id] = (sveza_igra, ZACETEK)
+
+        return nov_id
+
+    def ugibaj(self, id_igre, crka):
+        #Najdi
+        #Trenutno stanje nas ne zanima
+        igra, _ = self.igre[id_igre]
+        #Posodobi z delegiranjem
+        novo_stanje = igra.ugibaj(crka)
+        #Popravi v slovarju
+        self.igre[id_igre] = (igra, novo_stanje)
+
+        return novo_stanje
 
 class Igra:
     def __init__(self, geslo, crke=[]):
